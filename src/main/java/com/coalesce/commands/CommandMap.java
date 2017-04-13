cf4c19ce749a00ad067b31ec326a86d2098e63e8
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-13 19:45:55 +0200

    Formatting for days.

--- src/main/java/com/coalesce/commands/CommandMap.java
+++ src/main/java/com/coalesce/commands/CommandMap.java
@@ -10,46 +10,46 @@ import java.util.Map;
 
 public class CommandMap {
     private final Map<String, CommandEntry> entries = new LinkedHashMap<>();
-    
+
     CommandMap() {
         detectCommands();
     }
-    
+
     private void register(Class<? extends CommandExecutor> clazz) {
         CommandEntry entry = new CommandEntry(clazz);
-        
+
         registerCommand(entry.meta.name().toLowerCase(), entry);
-    
+
         Arrays.stream(entry.meta.aliases())
                 .map(String::toLowerCase)
                 .forEach(a -> registerCommand(a, entry));
     }
-    
+
     private void registerCommand(String label, CommandEntry entry) {
         entries.put(label, entry);
     }
-    
+
     public CommandEntry getEntry(String string) {
         return entries.get(string.toLowerCase());
     }
-    
+
     private void detectCommands() {
         Reflections ref = new Reflections(new ConfigurationBuilder()
                 .addScanners(new TypeAnnotationsScanner())
                 .forPackages(""));
-        
+
         ref.getSubTypesOf(CommandExecutor.class).forEach(this::register);
     }
-    
+
     public static class CommandEntry {
         final Class<? extends CommandExecutor> clazz;
         final Command meta;
-        
+
         private CommandEntry(Class<? extends CommandExecutor> clazz) {
             this.clazz = clazz;
             this.meta = clazz.getAnnotation(Command.class);
         }
-        
+
         public Command getMeta() {
             return meta;
         }
