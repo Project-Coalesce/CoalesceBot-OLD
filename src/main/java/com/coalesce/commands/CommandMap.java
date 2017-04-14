3cac43a3c0346369b9cf55eaf9c6e4ce5c4f193a
Lyxnx <a.julnes@gmail.com>
2017-04-14 15:43:51 +0100

    Use only one instance of each command class

--- src/main/java/com/coalesce/commands/CommandMap.java
+++ src/main/java/com/coalesce/commands/CommandMap.java
@@ -1,5 +1,6 @@
 package com.coalesce.commands;
 
+import com.coalesce.Bot;
 import org.reflections.Reflections;
 
 import java.util.Arrays;
@@ -7,46 +8,61 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CommandMap {
+    
+    private final Bot bot;
     private final Map<String, CommandEntry> entries = new LinkedHashMap<>();
-
-    CommandMap() {
+    
+    CommandMap(Bot bot) {
+        this.bot = bot;
+        
         detectCommands();
     }
-
+    
     private void register(Class<? extends CommandExecutor> clazz) {
-        System.out.println("Registering command: " + clazz.getSimpleName());
-        CommandEntry entry = new CommandEntry(clazz);
-
-        registerCommand(entry.meta.name().toLowerCase(), entry);
-
-        Arrays.stream(entry.meta.aliases())
-                .map(String::toLowerCase)
-                .forEach(a -> registerCommand(a, entry));
+        try {
+            System.out.println("Registering command: " + clazz.getSimpleName());
+            CommandEntry entry = new CommandEntry(clazz, this);
+            
+            registerCommand(entry.meta.name().toLowerCase(), entry);
+            
+            Arrays.stream(entry.meta.aliases())
+                    .map(String::toLowerCase)
+                    .forEach(a -> registerCommand(a, entry));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
+    
     private void registerCommand(String label, CommandEntry entry) {
         entries.put(label, entry);
     }
-
+    
     public CommandEntry getEntry(String string) {
         return entries.get(string.toLowerCase());
     }
-
+    
     private void detectCommands() {
         Reflections ref = new Reflections("com.coalesce.commands.executors");
-
+        
         ref.getSubTypesOf(CommandExecutor.class).forEach(this::register);
     }
-
-    public static class CommandEntry {
-        final Class<? extends CommandExecutor> clazz;
+    
+    public Map<String, CommandEntry> getEntries() {
+        return entries;
+    }
+    
+    public class CommandEntry {
+        final CommandExecutor executor;
         final Command meta;
-
-        private CommandEntry(Class<? extends CommandExecutor> clazz) {
-            this.clazz = clazz;
+        
+        private CommandEntry(Class<? extends CommandExecutor> clazz, CommandMap map) throws Exception {
+            this.executor = clazz.newInstance();
+            this.executor.jda = bot.getJDA();
+            this.executor.commandMap = map;
             this.meta = clazz.getAnnotation(Command.class);
+            this.executor.annotation = meta;
         }
-
+        
         public Command getMeta() {
             return meta;
         }
