f25b283c60d033dcaca94be277c740536024da69
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-16 00:00:02 +0200

    Correct regex.

--- src/main/java/com/coalesce/commands/executors/Boi.java
+++ src/main/java/com/coalesce/commands/executors/Boi.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.core.entities.MessageChannel;
 @Command(name = "boi", aliases = {"njsblessing"}, description = "Posts a boi meme into chat.", permission = "commands.boi")
 public class Boi extends CommandExecutor {
     @Override protected void execute(MessageChannel channel, Message message, String[] args) throws Exception {
-        String url = message.getAuthor().getName().matches("[0-9a-zA-Z]") ?
+        String url = message.getAuthor().getName().matches("(0-9a-zA-Z)") ?
                 "http://i1.kym-cdn.com/photos/images/newsfeed/001/183/604/ee9.png" :
                 "http://i.imgur.com/hOYMcij.jpg";
         throw new CommandError(url);
