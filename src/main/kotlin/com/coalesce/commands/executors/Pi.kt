da75beb4af05b6c58ad9a5eec74eb76a77af1378
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-16 23:15:51 +0200

    Replace CommandError with an embed.

--- src/main/kotlin/com/coalesce/commands/executors/Pi.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Pi.kt
@@ -1,14 +1,16 @@
 package com.coalesce.commands.executors
 
 import com.coalesce.commands.Command
-import com.coalesce.commands.CommandError
 import com.coalesce.commands.CommandExecutor
+import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.MessageChannel
+import java.awt.Color
+import java.util.concurrent.TimeUnit
 
 @Command(name = "Pi", permission = "commands.pi", description = "Shows pi.")
 class Pi : CommandExecutor() {
     override fun execute(channel: MessageChannel, message: Message, args: Array<String>) {
-        throw CommandError(Math.PI.toString())
+        message.channel.sendMessage(EmbedBuilder().setColor(Color(0.0f, 0.5f, 0.0f)).addField("Receiver", message.author.asMention, true).addField("Value of Pi", Math.PI.toString(), true).build()).queue { it.delete().queueAfter(15, TimeUnit.SECONDS) }
     }
 }
