1dff60dc2bab5be51b643bf70f49596b851ab95c
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-17 09:13:17 +0200

    Revert "4 hours is a long wait..."
    This reverts commit ee6ab05c5f7dbe860e967bc260aa73bee2ce8aa0.

--- src/main/kotlin/com/coalesce/commands/executors/Respects.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Respects.kt
@@ -10,6 +10,13 @@ import java.util.concurrent.TimeUnit
 @Command(name = "Respects", aliases = arrayOf("f", "nahusdream"), description = "Pays respects.", permission = "commands.respects")
 class Respects : CommandExecutor() {
     override fun execute(channel: MessageChannel, message: Message, args: Array<String>) {
+        if (Bot.instance.respectsLastUse != -1f) {
+            val lastUse = Bot.instance.respectsLastUse
+            if ((lastUse + TimeUnit.HOURS.toMillis(4.toLong())) > System.currentTimeMillis()) {
+                return
+            }
+        }
+        Bot.instance.respectsLastUse = System.currentTimeMillis().toFloat()
         channel.sendMessage("Respects have been paid!").queue { it.delete().queueAfter(1, TimeUnit.MINUTES) }
     }
 }
