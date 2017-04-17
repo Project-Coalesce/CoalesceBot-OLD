5d5718e952be1fa35bd4c73ecf5ae24f1859ef59
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-17 09:39:19 +0200

    Save last paid respects millis.

--- src/main/kotlin/com/coalesce/Bot.kt
+++ src/main/kotlin/com/coalesce/Bot.kt
@@ -6,6 +6,9 @@ import net.dv8tion.jda.core.AccountType
 import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.JDABuilder
 import net.dv8tion.jda.core.entities.Game
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -24,6 +27,13 @@ class Bot {
                 println("The data directory didn't exist already and was created.")
             }
         }
+        val data = File(DATA_DIRECTORY, "data.json")
+        if (data.exists()) {
+            data.reader().use {
+                respectsLastUse = Constants.GSON.fromJson(it, mapOf<String, Any?>()::class.java)["respectsLastUse"] as Float
+            }
+        }
+        Runtime.getRuntime().addShutdownHook(Shutdown())
         jda = JDABuilder(AccountType.BOT).setAudioEnabled(false).setCorePoolSize(4).setToken(token).buildBlocking()
         jda.guilds.map { it.publicChannel }.filter { it.canTalk() }.forEach { it.sendMessage("The bot is now enabled and ready for user input.").queue { it.delete().queueAfter(5, TimeUnit.SECONDS) } }
 
@@ -32,7 +42,19 @@ class Bot {
         jda.addEventListener(CommandListener())
     }
 
+    class Shutdown : Thread() {
+        override fun run() {
+            val data = File(DATA_DIRECTORY, "data.json")
+            if (data.exists()) {
+                data.delete()
+            }
+            Files.write(data.toPath(), Constants.GSON.toJson(mapOf("respectsLastUse" to Bot.instance.respectsLastUse)).toByteArray(), StandardOpenOption.CREATE, StandardOpenOption.WRITE)
+            Bot.instance.jda.shutdown(true)
+        }
+    }
+
     companion object {
         lateinit var instance: Bot
     }
 }
+
