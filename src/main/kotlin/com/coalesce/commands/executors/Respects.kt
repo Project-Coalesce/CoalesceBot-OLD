36af3865b11a612371d1dfb793e81cca1c82712f
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-17 09:39:38 +0200

    Increase cooldown time for respects and add a message which tells next use.

--- src/main/kotlin/com/coalesce/commands/executors/Respects.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Respects.kt
@@ -1,6 +1,7 @@
 package com.coalesce.commands.executors
 
 import com.coalesce.Bot
+import com.coalesce.Constants
 import com.coalesce.commands.Command
 import com.coalesce.commands.CommandExecutor
 import net.dv8tion.jda.core.entities.Message
@@ -12,7 +13,9 @@ class Respects : CommandExecutor() {
     override fun execute(channel: MessageChannel, message: Message, args: Array<String>) {
         if (Bot.instance.respectsLastUse != -1f) {
             val lastUse = Bot.instance.respectsLastUse
-            if ((lastUse + TimeUnit.HOURS.toMillis(4.toLong())) > System.currentTimeMillis()) {
+            if ((lastUse + TimeUnit.HOURS.toMillis(6.toLong())) > System.currentTimeMillis()) {
+                channel.sendMessage("You can pay respects again in ${Constants.DECIMAL_FORMAT.format(TimeUnit.MILLISECONDS.toHours(((lastUse + TimeUnit.HOURS.toMillis(6)-System.currentTimeMillis()).toLong())))} hours.")
+                        .queue { it.delete().queueAfter(5, TimeUnit.SECONDS) }
                 return
             }
         }
