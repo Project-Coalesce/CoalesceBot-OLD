3f1ff490c88e92ed07730fca34051ef9542e519b
deprilula28 <deprilula28@gmail.com>
2017-04-17 20:18:16 -0300

    Add moderator role permission check

--- src/main/kotlin/com/coalesce/commands/executors/Punish.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Punish.kt
@@ -1,5 +1,6 @@
 package com.coalesce.commands.executors
 
+import com.coalesce.Bot
 import com.coalesce.commands.Command
 import com.coalesce.commands.CommandExecutor
 import com.coalesce.punishments.Punishment
@@ -8,17 +9,23 @@ import com.coalesce.punishments.Reason
 import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.MessageChannel
 
-@Command(name = "Punish", permission = "commands.punish", description = "Punishes users")
+@Command(name = "Punish", permission = "commands.punish", description = "Automatically creates a punishment and server logs it")
 class Punish : CommandExecutor() {
     val manager = PunishmentManager()
 
     override fun execute(channel: MessageChannel, message: Message, args: Array<String>) {
+        if(!message.guild.getMember(message.author).roles.contains(Bot.instance.jda.getRoleById("268239031467376640"))){
+            channel.sendMessage("You lack permission to use this command.").queue()
+            return
+        }
+
         if(message.mentionedUsers.size != 0){
             channel.sendMessage("You need to mention an user to use this command.").queue()
             return
         }
+
         if(args.size < 2){
-            channel.sendMessage("Invalid usage, usage is: !punish <mention> <reason> [description]").queue()
+            channel.sendMessage("Invalid usage, proper usage is: !punish <mention> <reason> [description]").queue()
             return
         }
 
@@ -28,7 +35,7 @@ class Punish : CommandExecutor() {
             reason = Reason.valueOf(args[1])
         } catch (e: Exception) {
             val errorMessages = StringBuilder()
-            Reason.values().forEach { errorMessages.append(it.toString() + " (" + it.description + ") ") }
+            Reason.values().forEach { errorMessages.append(it.toString() + " (" + it.description + " | Severity " + it.severity + ") ") }
 
             channel.sendMessage("That reason does not exist. Here's a list of valid reasons:\n" + errorMessages.toString()).queue()
             return
