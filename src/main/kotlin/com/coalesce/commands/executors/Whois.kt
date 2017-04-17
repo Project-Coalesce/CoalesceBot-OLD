92823fb3f373668a645a04a67042656b3096d446
Mariell Hoversholm <proximyst@proximyst.com>
2017-04-17 09:13:29 +0200

    Revert "Changed a String... That's it..."
    This reverts commit 72c8bdecafb10a32e1aa38bf84a7a90284598711.

--- src/main/kotlin/com/coalesce/commands/executors/Whois.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Whois.kt
@@ -33,7 +33,7 @@ class Whois : CommandExecutor() {
                 .addField("Type", if (member.user.isBot) "Bot" else if (member.isOwner) "Owner" else "User", true)
                 .addField("Creation Time", member.user.creationTime.format(DateTimeFormatter.ofPattern("d MMM uuuu")), true)
         if (member.roles.size == 1 && member.roles[0].name == "Python") {
-            builder.addField("Has ugly yellow color?", "Sadly, yes", true)
+            builder.addField("Has ugly yellow colour?", "Sadly, yes", true)
         }
         message.channel.sendMessage(builder.build()).queue { it.delete().queueAfter(30, TimeUnit.SECONDS) }
     }
