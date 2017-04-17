715926a4a113d9ee5e5ff565494264d81b6cce26
deprilula28 <deprilula28@gmail.com>
2017-04-17 16:54:55 -0300

    Add permanent

--- src/main/kotlin/com/coalesce/punishments/PunishmentTimes.kt
+++ src/main/kotlin/com/coalesce/punishments/PunishmentTimes.kt
@@ -5,6 +5,6 @@ import java.util.*
 enum class PunishmentTimes(var timeUnit: Int, var time: Int) {
 
     WARNING(-1, -1), SECOND_WARNING(-1, -1), THIRD(Calendar.HOUR, 1), FOURTH(Calendar.HOUR, 8), FIFTH(Calendar.DAY_OF_MONTH, 2),
-    SIXTH(Calendar.WEEK_OF_MONTH, 1)
+    SIXTH(Calendar.WEEK_OF_MONTH, 1), SEVENTH(Calendar.YEAR, 666)
 
 }
