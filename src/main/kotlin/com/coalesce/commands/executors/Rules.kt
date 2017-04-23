709ece40649ce0bb6d69405fa9645279d112d4c7
Proximyst <proximyst@proximyst.com>
2017-04-23 22:03:43 +0200

    Fix class name and direct link to the image

--- src/main/kotlin/com/coalesce/commands/executors/Rules.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Rules.kt
@@ -8,9 +8,9 @@ import net.dv8tion.jda.core.entities.MessageChannel
 import java.util.concurrent.TimeUnit
 
 @Command(name = "Rules", aliases = {"Rulez"}, permission = "commands.rules", description = "Hey you... Yes, you! Want to see the rules?")
-class Pi : CommandExecutor() {
+class Rules : CommandExecutor() {
     override fun execute(channel: MessageChannel, message: Message, args: Array<String>) {                    
           event.channel.sendMessage(MessageBuilder().append(event.message.author).appendFormat(": Ptssss, you... Head over to %s\n%s", event.guild.getTextChannelById(269178364483338250L).asMention,
-                 "http://m.imgur.com/B50EQKp?r").build()).queue { it.delete().queueAfter(10, TimeUnit.SECONDS) }
+                 "http://i.imgur.com/B50EQKp.png").build()).queue { it.delete().queueAfter(10, TimeUnit.SECONDS) }
     }
 }
