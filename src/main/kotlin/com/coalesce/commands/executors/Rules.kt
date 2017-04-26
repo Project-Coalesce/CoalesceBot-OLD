4e104b8e97a54d5c293385bd8c3283e121636477
NahuLD <nahueldolores@hotmail.com>
2017-04-25 23:00:24 -0300

    Fixed Rules, and hijacked the cheater

--- src/main/kotlin/com/coalesce/commands/executors/Rules.kt
+++ src/main/kotlin/com/coalesce/commands/executors/Rules.kt
@@ -2,15 +2,15 @@ package com.coalesce.commands.executors
 
 import com.coalesce.commands.Command
 import com.coalesce.commands.CommandExecutor
-import net.dv8tion.jda.core.EmbedBuilder
+import net.dv8tion.jda.core.MessageBuilder
 import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.MessageChannel
 import java.util.concurrent.TimeUnit
 
-@Command(name = "Rules", aliases = {"Rulez"}, permission = "commands.rules", description = "Hey you... Yes, you! Want to see the rules?")
+@Command(name = "Rules", aliases = arrayOf("Rulez"), permission = "commands.rules", description = "Hey you... Yes, you! Want to see the rules?")
 class Rules : CommandExecutor() {
     override fun execute(channel: MessageChannel, message: Message, args: Array<String>) {                    
-          channel.sendMessage(MessageBuilder().append(event.message.author).appendFormat(": Ptssss, you... Head over to %s\n%s", event.guild.getTextChannelById(269178364483338250L).asMention,
+          channel.sendMessage(MessageBuilder().append(message.author).appendFormat(": Ptssss, you... Head over to %s\n%s", "<#269178364483338250>",
                  "http://i.imgur.com/B50EQKp.png").build()).queue { it.delete().queueAfter(10, TimeUnit.SECONDS) }
     }
 }
