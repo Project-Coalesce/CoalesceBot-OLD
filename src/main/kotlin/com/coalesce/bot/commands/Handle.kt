db50aabc3fe8791f3fb5ae8ecc849f1d37d9a787
deprilula28 <deprilula28@gmail.com>
2017-05-14 21:38:59 -0300

    Patched cooldowns (again)

--- src/main/kotlin/com/coalesce/bot/commands/Handle.kt
+++ src/main/kotlin/com/coalesce/bot/commands/Handle.kt
@@ -52,7 +52,8 @@ class Listener internal constructor() : ListenerAdapter(), Embeddables {
                     if (current != null) {
                         if (current > System.currentTimeMillis()) {
                             // TODO: Prettify current seconds
-                            val remaining = (cooldown - System.currentTimeMillis()).toLong()
+                            val remaining = (current.toLong() - System.currentTimeMillis())
+                            println(System.currentTimeMillis().toString() + ", " + current.toLong() + ", " + remaining)
                             it(embed().setColor(Color(204, 36, 24)).setAuthor(it.message.author.name, null, it.message.author.avatarUrl).setTitle("Cooldown", null)
                                     .setDescription("That command is on global cooldown for ${prettify(remaining)}."))
                             return@Predicate false
@@ -82,7 +83,7 @@ class Listener internal constructor() : ListenerAdapter(), Embeddables {
                     if (userCooldown != null) {
                         if (userCooldown > System.currentTimeMillis()) {
                             // TODO: Add time left to the message.
-                            val remaining = (cooldown - System.currentTimeMillis()).toLong()
+                            val remaining = (userCooldown.toLong() - System.currentTimeMillis())
                             it(embed().setColor(Color(204, 36, 24)).setAuthor(it.message.author.name, null, it.message.author.avatarUrl).setTitle("Cooldown", null)
                                     .setDescription("That command is on cooldown for ${prettify(remaining)}."))
                             return@Predicate false
@@ -104,13 +105,17 @@ class Listener internal constructor() : ListenerAdapter(), Embeddables {
         val second = timeDiff / 1000 % 60
         val minute = timeDiff / (1000 * 60) % 60
         val hour = timeDiff / (1000 * 60 * 60) % 24
-        val day = timeDiff / (1000 * 60 * 60 * 24) % 30
+        val day = timeDiff / (1000 * 60 * 60 * 24)
 
-        if (day > 0) return day.toString() + " " + if (day > 1) "days" else "day"
-        if (hour > 0) return hour.toString() + " " + if (hour > 1) "hours" else "hour"
-        if (minute > 0) return minute.toString() + " " + if (minute > 1) "minutes" else "minute"
-        if (second > 0) return second.toString() + " " + if (second > 1) "seconds" else "second"
+        if (day > 0) return "$day${ensurePlural(day, "day")} and $hour${ensurePlural(hour, "hour")}"
+        if (hour > 0) return "$hour${ensurePlural(hour, "hour")} and $minute${ensurePlural(minute, "minute")}"
+        if (minute > 0) return "$minute${ensurePlural(minute, "minute")} and $second${ensurePlural(second, "second")}"
+        if (second > 0) return "$second${ensurePlural(second, "second")}"
         return timeDiff.toString() + "ms"
+    }
+
+    fun ensurePlural(numb: Long, str: String): String {
+        return if (numb > 1) " ${str}s" else " $str"
     }
 
     override fun onGenericEvent(event: Event) {
