0aaa2186dbd3a85b16a8eebeb669e53bb18675ba
deprilula28 <deprilula28@gmail.com>
2017-05-20 22:36:46 -0300

    Change permissions and add permissions command

---
+++ src/main/kotlin/com/coalesce/bot/commands/executors/Permission.kt
@@ -0,0 +1,93 @@
+package com.coalesce.bot.commands.executors
+
+import com.coalesce.bot.Main
+import com.coalesce.bot.commands.CommandType
+import com.coalesce.bot.commands.RootCommand
+import com.coalesce.bot.commands.RootCommandContext
+import com.coalesce.bot.permissions.WrappedRole
+import com.coalesce.bot.permissions.WrappedUser
+import com.google.inject.Inject
+import net.dv8tion.jda.core.entities.Member
+import net.dv8tion.jda.core.entities.Role
+import net.dv8tion.jda.core.entities.User
+
+class Permission @Inject constructor(val bot: Main) {
+    private val perms = bot.listener.perms
+
+    @RootCommand(
+            name = "Permission",
+            type = CommandType.ADMINISTRATION,
+            permission = "commands.permissions",
+            description = "Allows for changing permissions. Roles with 'Administrator' permissions and guild creators have all permissions.",
+            cooldownSubs = false
+    )
+    fun execute(context: RootCommandContext) {
+        if (context.args.size < 2) {
+            context("* Invalid Syntax.")
+            return
+        }
+
+        val perm = context.args[1]
+        val value: Boolean?
+
+        if (context.args.size >= 3) {
+            value = context.args[2].toBoolean()
+        } else {
+            value = null
+        }
+
+        if (context.message.mentionedUsers.isNotEmpty()) {
+            changePermissionForUser(context.message.guild.getMember(context.message.author), perm, value)
+        } else if (context.message.mentionedRoles.isNotEmpty()) {
+            changePermissionForRole(context.message.mentionedRoles.first(), perm, value)
+        } else if (context.args.isNotEmpty()) {
+            val roles = bot.jda.getRolesByName(context.args.first(), true)
+
+            if (roles.isEmpty()) {
+                context("* The entered argument for what the change permission does not exist.")
+                return
+            }
+
+            val role = roles.first()
+            changePermissionForRole(role, perm, value)
+        }
+    }
+
+    fun changePermissionForRole(role: Role, perm: String, value: Boolean?) {
+        val wrappedRole = findWrappedRole(role)
+        wrappedRole.permissions[perm] = value ?: !(wrappedRole.permissions[perm] ?: false)
+    }
+
+    fun findWrappedRole(role: Role): WrappedRole {
+        val wrappedRole = perms.ranks[role.idLong]
+        if (wrappedRole == null) {
+            perms.ranks[role.idLong] = WrappedRole(role)
+            return perms.ranks[role.idLong]!!
+        }
+        return wrappedRole
+    }
+
+    fun changePermissionForUser(member: Member, perm: String, value: Boolean?) {
+        val guildMap = findGuildMap(member)
+        guildMap[perm] = value ?: !(guildMap[perm] ?: false)
+    }
+
+    fun findGuildMap(member: Member): MutableMap<String, Boolean> {
+        val wrappedUser = findWrappedUser(member.user)
+        val guildMap = wrappedUser.permissions[member.guild]
+        if (guildMap == null) {
+            wrappedUser.permissions[member.guild] = mutableMapOf()
+            return wrappedUser.permissions[member.guild]!!
+        }
+        return guildMap
+    }
+
+    fun findWrappedUser(user: User): WrappedUser {
+        val wrappedUser = perms.users[user.idLong]
+        if (wrappedUser == null) {
+            perms.users[user.idLong] = WrappedUser(user)
+            return perms.users[user.idLong]!!
+        }
+        return wrappedUser
+    }
+}
--- src/main/kotlin/com/coalesce/bot/permissions/Handle.kt
+++ src/main/kotlin/com/coalesce/bot/permissions/Handle.kt
@@ -2,13 +2,14 @@ package com.coalesce.bot.permissions
 
 import com.coalesce.bot.utilities.hashTableOf
 import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.core.Permission
 import net.dv8tion.jda.core.entities.Member
 import java.util.*
 import java.util.stream.Collectors
 
 class RankManager internal constructor(jda: JDA) {
-    private val ranks = hashTableOf<Long, WrappedRole>()
-    private val users = hashTableOf<Long, WrappedUser>()
+    val ranks = hashTableOf<Long, WrappedRole>()
+    val users = hashTableOf<Long, WrappedUser>()
 
     init {
         jda.guilds.map { it.members }.forEach { it.forEach { users.put(it.user.idLong, WrappedUser(it.user)) } }
@@ -17,15 +18,23 @@ class RankManager internal constructor(jda: JDA) {
     fun getPermissions(member: Member): Map<String, Boolean> {
         val user = users.filter { it == member.user }.values.firstOrNull() ?: return mapOf()
         val applicablePerms = mutableMapOf<String, Boolean>()
+
         ranks.entries.stream()
-                .filter { it -> member.roles.contains(it.value.role) }
-                .sorted().collect(Collectors.toList()).reversed().stream().forEachOrdered { it -> applicablePerms.putAll(it.value.permissions) }
+                .filter { member.roles.contains(it.value.role) }
+                .sorted().collect(Collectors.toList()).reversed().stream().forEachOrdered {applicablePerms.putAll(it.value.permissions) }
         applicablePerms.putAll(user.permissions[member.guild] ?: return applicablePerms)
+
         return applicablePerms
     }
 
     fun hasPermission(member: Member, vararg permissions: String): Boolean {
         if (permissions.isEmpty()) {
+            return true
+        }
+        if (member.guild.owner == member) {
+            return true
+        }
+        if (member.roles.stream().anyMatch { it.hasPermission(Permission.ADMINISTRATOR) }) {
             return true
         }
         val permsMap = getPermissions(member)
