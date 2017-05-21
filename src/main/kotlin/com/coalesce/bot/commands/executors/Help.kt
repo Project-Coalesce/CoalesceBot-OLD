633d51a382dad181bcf71be7dea70ce6cdf2eb09
deprilula28 <deprilula28@gmail.com>
2017-05-20 23:15:32 -0300

    Fix help

--- src/main/kotlin/com/coalesce/bot/commands/executors/Help.kt
+++ src/main/kotlin/com/coalesce/bot/commands/executors/Help.kt
@@ -6,6 +6,7 @@ import com.coalesce.bot.canDelete
 import com.coalesce.bot.commands.*
 import com.coalesce.bot.utilities.ifwithDo
 import com.google.inject.Inject
+import java.nio.file.Files.delete
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -18,32 +19,31 @@ class Help @Inject constructor(val bot: Main) : Embeddables {
             globalCooldown = 35.0
     )
     fun execute(context: RootCommandContext) {
-        val embed = embed().data("Help", colour = Colour.GREEN)
         val map = mutableMapOf<CommandType, MutableSet<CommandEntry>>()
         bot.listener.registry.commands.forEach { _, value ->
             if (value.rootAnnotation.type != CommandType.HIDDEN) {
                 map[value.rootAnnotation.type] = map[value.rootAnnotation.type]?.apply { add(value) } ?: mutableSetOf()
             }
         }
-        val member = context.message.guild.getMember(context.message.author)
+
         val out = mutableMapOf<CommandType, String>()
         map.forEach { type, entries ->
             val builder = StringBuilder()
             entries.forEach {
                 builder.append("`${if (it.rootAnnotation.name == "xD") "xD" else it.rootAnnotation.name.capitalize()}`")
-                builder.append(" (${it.rootAnnotation.usage}) ")
+                builder.append(" ${it.rootAnnotation.usage} ")
                 if (it.rootAnnotation.aliases.isNotEmpty()) {
                     builder.append(Arrays.toString(it.rootAnnotation.aliases.map { it.capitalize() }.toTypedArray()))
                 }
-                builder.append(": ${it.rootAnnotation.description}\n ${if (bot.listener.perms.hasPermission(member,
-                        it.rootAnnotation.permission)) "<:permissable:315634057625075712>" else "<:no_permission:315617783738007552>"}")
+                builder.append(": ${it.rootAnnotation.description}\n")
             }
             out[type] = builder.toString().trim()
         }
 
+        val messageBuilder = StringBuilder("***Help***\n\n")
         out.forEach { k, v ->
-            embed.field(k.name.toLowerCase().capitalize(), v, true)
+            messageBuilder.append("\n\n**${k.name.toLowerCase().capitalize()}:**\n$v")
         }
-        context(embed) { ifwithDo(canDelete, context.message.guild) { delete().queueAfter(45, TimeUnit.SECONDS) } }
+        context(messageBuilder.toString()) { ifwithDo(canDelete, context.message.guild) { delete().queueAfter(45, TimeUnit.SECONDS) } }
     }
 }
