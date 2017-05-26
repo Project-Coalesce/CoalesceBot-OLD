d7eed6113f39723b195e5f43e3d4120d8058f089
NahuLD <nahueldolores@hotmail.com>
2017-05-26 01:24:56 -0300

    Playing command

---
+++ src/main/kotlin/com/coalesce/bot/commands/executors/Playing.kt
@@ -0,0 +1,25 @@
+package com.coalesce.bot.commands.executors
+
+import com.coalesce.bot.commands.CommandType
+import com.coalesce.bot.commands.RootCommand
+import com.coalesce.bot.commands.RootCommandContext
+import net.dv8tion.jda.core.entities.Game
+
+class Playing {
+
+    @RootCommand(
+            name = "playing",
+            permission = "command.playing",
+            type = CommandType.DEBUG,
+            description = "Change bot game message"
+    )
+    fun execute(context: RootCommandContext) {
+        if (context.args.isEmpty()) {
+            context(context.author, "Usage: `!playing <text>`")
+            return
+        }
+        val status = context.args.joinToString(separator = " ")
+        context("* Game changed to: $status")
+        context.jda.presence.game = Game.of(status)
+    }
+}
