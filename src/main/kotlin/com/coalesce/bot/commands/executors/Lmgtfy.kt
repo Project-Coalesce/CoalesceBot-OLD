ea716a67ba41aa3bc9af5d80971fa566a3b9be3a
NahuLD <nahueldolores@hotmail.com>
2017-06-08 04:41:49 -0300

    LMGTFY changes

--- src/main/kotlin/com/coalesce/bot/commands/executors/Lmgtfy.kt
+++ src/main/kotlin/com/coalesce/bot/commands/executors/Lmgtfy.kt
@@ -3,7 +3,9 @@ package com.coalesce.bot.commands.executors
 import com.coalesce.bot.commands.CommandType
 import com.coalesce.bot.commands.RootCommand
 import com.coalesce.bot.commands.RootCommandContext
+import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.User
+import java.util.concurrent.TimeUnit
 
 class Lmgtfy {
     @RootCommand(
@@ -13,7 +15,7 @@ class Lmgtfy {
             permission = "commands.lmgtfy",
             usage = "<user : optional> <phrase>",
             globalCooldown = 5.0,
-            type = CommandType.INFORMATION
+            type = CommandType.FUN
     )
     fun execute(context: RootCommandContext) {
         if (context.args.isEmpty()) {
@@ -22,20 +24,16 @@ class Lmgtfy {
         }
 
         if (context.message.mentionsEveryone() || context.message.mentionedUsers.isNotEmpty() || context.message.mentionedRoles.isNotEmpty()) {
-            context(context.author, "You can't tag roles, users or @everyone.")
+            context(context.author, "You can't tag roles, users or everyone.")
             return
         }
-
-        val arguments: Array<String>
-        var member: User? = context.message.mentionedUsers.firstOrNull()
-        if (member == null) {
-            member = context.author
-            arguments = context.args
-        } else {
-            arguments = context.args.copyOfRange(1, context.args.size)
-        }
-
-        val phrase = arguments.joinToString(separator = "+")
-        context(member, "Have you tried Googling it? http://lmgtfy.com/?q=$phrase")
+        val phrase = context.args.joinToString(separator = "+")
+        context.channel.sendMessage(
+                EmbedBuilder().apply {
+                    setTitle("Have you tried Googling it?", "http://lmgtfy.com/?q=$phrase")
+                    setAuthor(context.author.name, null, context.author.avatarUrl)
+                    setDescription("Click the title for more details")
+                }.build()
+        ).queue{ it.delete().queueAfter(25, TimeUnit.SECONDS) }
     }
 }
