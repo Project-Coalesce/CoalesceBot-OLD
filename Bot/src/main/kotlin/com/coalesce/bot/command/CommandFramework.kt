e153998828806b5e27d542cd9e113d4cf68efaab
deprilula28 <deprilula28@gmail.com>
2017-07-09 18:27:06 -0300

    :fire:fix

--- Bot/src/main/kotlin/com/coalesce/bot/command/CommandFramework.kt
+++ Bot/src/main/kotlin/com/coalesce/bot/command/CommandFramework.kt
@@ -2,7 +2,6 @@ package com.coalesce.bot.command
 
 import com.coalesce.bot.Main
 import com.coalesce.bot.command.handlers.Help
-import com.coalesce.bot.command.language.LanguageManager
 import com.coalesce.bot.commandPrefix
 import com.coalesce.bot.commandPrefixLen
 import com.coalesce.bot.utilities.*
