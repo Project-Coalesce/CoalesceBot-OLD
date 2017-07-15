b71e9aaaef212ee8469745a2848423ad42b99096
NahuLD <nahueldolores@hotmail.com>
2017-07-15 14:03:46 -0300

    Proximyst command

---
+++ Miscellaneous/src/main/kotlin/com/coalesce/bot/misc/commands/Proximyst.kt
@@ -0,0 +1,16 @@
+package com.coalesce.bot.misc.commands
+
+import com.coalesce.bot.command.Command
+import com.coalesce.bot.command.CommandAlias
+import com.coalesce.bot.command.CommandContext
+
+@Command("Proximyst", "proxi bae")
+class Proximyst {
+
+    @CommandAlias("You will be remembered")
+    fun exectue(context: CommandContext) {
+        context("Proximyst was a good gal. She helped out a bunch of people, and created this bot." +
+                "\nWe will miss you, we hope you rest in peace." +
+                "\nhttp://i.imgur.com/lAkpLgC.png")
+    }
+}
